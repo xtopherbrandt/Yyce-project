fae3958b2b7e1f7689f8c2f736198efefc4f87e6
Chris Brandt <xtopher.brandt@gmail.com>
2023-07-12 15:25:45 -0600

    fix: remove TODO notes

--- controllers/v1_controllers.py
+++ controllers/v1_controllers.py
@@ -47,9 +47,6 @@ def venues():
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for Hop should return "The Musical Hop".
-  # search for "Music" should return "The Musical Hop" and "Park Square Live Music & Coffee"
   response={
     "count": 1,
     "data": [{
@@ -193,9 +190,6 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
   response={
     "count": 1,
     "data": [{
--- controllers/v2_controllers.py
+++ controllers/v2_controllers.py
@@ -72,9 +72,6 @@ def artists_v2():
 
 @app.route('/V2/artists/search', methods=['POST'])
 def search_artists_v2():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
     
   search_term = request.form.get('search_term', '')
   
