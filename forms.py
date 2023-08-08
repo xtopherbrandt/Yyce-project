8567bcda7bd38f5fea4b8b3bdb4fa22c5a0415de
Chris Brandt <xtopher.brandt@gmail.com>
2023-08-08 16:14:23 -0700

    feat: restrict venue links to url

--- forms.py
+++ forms.py
@@ -86,7 +86,7 @@ class VenueForm(Form):
         'phone'
     )
     image_link = StringField(
-        'image_link'
+        'image_link', validators=[URL()]
     )
     genres = SelectMultipleField(
         # TODO implement enum restriction
@@ -117,7 +117,7 @@ class VenueForm(Form):
         'facebook_link', validators=[URL()]
     )
     website_link = StringField(
-        'website_link'
+        'website_link', validators=[URL()]
     )
 
     seeking_talent = BooleanField( 'seeking_talent' )
