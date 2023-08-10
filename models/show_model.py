508495dc4554a0763dc671c4ce7de512c542b100
Chris Brandt <xtopher.brandt@gmail.com>
2023-08-10 13:07:20 -0700

    fix: show_datetime is a PK

--- models/show_model.py
+++ models/show_model.py
@@ -10,7 +10,7 @@ class Show(db.Model):
 
     venue_id = Column(Integer, ForeignKey("Venue.id"), primary_key=True)
     artist_id = Column(Integer, ForeignKey("Artist.id"), primary_key=True)
-    show_datetime = Column(DateTime, nullable=False)
+    show_datetime = Column(DateTime, primary_key=True)
     
     def __repr__(self):
         return f'<Show Venue: {self.venue_id} Artist: {self.artist_id} Show Time: {self.show_datetime}>'
